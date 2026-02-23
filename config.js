d91047da38efcad6af5a98f82d66b63cd698e219
sixteeninter-ux <sixteeninter@gmail.com>
2026-02-23 18:43:45 +0700

    Add application configuration for BSC Mainnet

---
+++ config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  CHAIN_ID_HEX: "0x38",
+  CHAIN_NAME: "BSC Mainnet",
+  EXPLORER: "https://bscscan.com",
+
+  CONTRACT: "0xe1686D3133c273F54a5584Fd6aFbb68f4eEb5D89",
+  STC: "0x12730990De18c6864e45d22f007EBebaFbf6c76b"
+};
