49a08f771059b3440ec3168b3ec1b9558b2a0742
sixteeninter-ux <sixteeninter@gmail.com>
2026-02-23 19:10:35 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  CHAIN_ID_HEX: "0x38",
+  CHAIN_NAME: "BSC Mainnet",
+  RPC_URL: "https://bsc-dataseed.binance.org/",
+  EXPLORER: "https://bscscan.com",
+
+  CONTRACT: "0xe1686D3133c273F54a5584Fd6aFbb68f4eEb5D89",
+  STC: "0x12730990De18c6864e45d22f007EBebaFbf6c76b"
+};
